e48e8a82dc723216fd4102da3ae385def62a58cd
Markus Mayer <development@widemeadows.de>
2014-04-13 11:42:14 +0200

    initial commit

---
+++ src/Model/IRandomNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace widemeadows.ml.kohonen.model
+{
+    /// <summary>
+    /// Interface IRandomNumber
+    /// </summary>
+    public interface IRandomNumber
+    {
+        /// <summary>
+        /// Sets the seed for the generator.
+        /// </summary>
+        /// <param name="seed">The seed.</param>
+        void SetSeed(int seed);
+
+        /// <summary>
+        /// Gets a <see cref="System.Double"/> value in the range of <paramref name="min"/> to <paramref name="max"/>.
+        /// </summary>
+        /// <param name="min">The minimum value (inclusive).</param>
+        /// <param name="max">The maximum value (inclusive).</param>
+        /// <returns>System.Double.</returns>
+        double GetDouble(double min = -1, double max = 1);
+    }
+}
