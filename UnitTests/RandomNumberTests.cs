5aec7c709f2e54d3a39aba3f0d279ed12c01814a
Markus Mayer <development@widemeadows.de>
2014-04-13 12:25:12 +0200

    added a simple unit test for the XOR shift

---
+++ UnitTests/RandomNumberTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using RandomNumberGenerator;
+
+namespace widemeadows.ml.kohonen.tests
+{
+    [TestFixture]
+    public sealed class RandomNumberTests
+    {
+        [Test]
+        public void XorShiftDoesNotRepeadedlyReturnTheSameNumber()
+        {
+            var rng = new XorShiftRng();
+            const int iterations = 20;
+            double lastNumber = Double.NaN;
+            int count = 0;
+
+            // generate multiple random numbers
+            for (int i = 0; i < iterations; ++i)
+            {
+                var number = rng.GetDouble(min:-1.0, max:1.0);
+                number.Should().BeGreaterOrEqualTo(-1.0, "because that is the lower bound");
+                number.Should().BeLessOrEqualTo(1.0, "because that is the upper bound");
+
+                // increase count if numbers are the same, otherwise reset counter
+                if (number.Equals(lastNumber)) ++count;
+                else count = 0;
+
+                // store number
+                lastNumber = number;
+            }
+
+            count.Should().BeLessOrEqualTo(5, "because more than five times the same number is highly unlikely");
+        }
+    }
+}
